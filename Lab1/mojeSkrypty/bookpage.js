ee9e78e4b846676ec8cc827475f306d098e32cd1
szadzia98 <szadzia98@gmail.com>
2020-10-28 23:08:07 +0100

    Projektowanie stron internetowych z bootstrap

---
+++ Lab1/mojeSkrypty/bookpage.js
@@ -0,0 +1,24 @@
+let kartkaZPodrecznika = document.getElementById("kartka");
+kartkaZPodrecznika.style.border = "3px solid black";
+kartkaZPodrecznika.style.backgroundColor = "#fff";
+kartkaZPodrecznika.style.fontSize = "15px";
+kartkaZPodrecznika.style.fontFamily = "Arial";
+
+
+
+let wzoryMargin = document.getElementById("wzory-marign-left");
+wzoryMargin.style.marginLeft = "-66px";
+wzoryMargin.style.fontFamily = "Arial";
+wzoryMargin.style.fontSize = "15px";
+
+ let tabelka = document.getElementsByTagName("table");
+ tabelka[0].style.border = "1px solid black";
+
+let tdTab = document.getElementsByTagName("td");
+for(let i=0;i<tdTab.length;i++)
+{
+    tdTab[i].style.border = "1px solid black";
+}
+
+
+
